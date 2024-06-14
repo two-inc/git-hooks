273644a8094d9868863ade81d3b58173a0784226
Bharat Kunwar <bkunwar@two.inc>
2024-06-14 22:28:32 +0100

    Bump version 24.05.23-2 -> 24.06.14

--- commit-msg.py
+++ commit-msg.py
@@ -22,7 +22,7 @@ and a conventional commit type, e.g.
 
     T-5482/feat: Amazing new feature
 
-See https://github.com/two-inc/git-hooks/blob/24.05.23-2/README.md for more info."
+See https://github.com/two-inc/git-hooks/blob/24.06.14/README.md for more info."
 """
 
 if __name__ == "__main__":
